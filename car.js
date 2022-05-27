6d9c475693f269501910db1b217535b34367a2ea
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-05-27 15:43:36 -0400

    added an angle property to the car class

--- car.js
+++ car.js
@@ -9,6 +9,7 @@ class Car{
         this.acceleration=0.2;
         this.maxSpeed=3;
         this.friction=0.05;
+        this.angle=0;
 
         this.controls = new Controls();
     }
@@ -20,7 +21,7 @@ class Car{
         if(this.controls.reverse){
             this.speed-=this.acceleration;
         }
-        if(this.speed<-this.maxSpeed){
+        if(this.speed>this.maxSpeed){
             this.speed=this.maxSpeed;
         }
         if(this.speed<-this.maxSpeed/2){
@@ -32,17 +33,32 @@ class Car{
         if(this.speed<0){
             this.speed+=this.friction;
         }
+        if(Math.abs(this.speed)<this.friction){
+            this.speed=0;
+        }
+        if(this.controls.left){
+            this.angle+=0.03;
+        }
+        if(this.controls.right){
+            this.angle-=0.03;
+        }
         this.y-=this.speed;
     }
 
     draw(ctx){
+        ctx.save();
+        ctx.translate(this.x,this.y);
+        ctx.rotate(-this.angle);
+
         ctx.beginPath();
         ctx.rect(
-            this.x - this.width/2,
-            this.y - this.height/2,
+            -this.width/2,
+            -this.height/2,
             this.width,
             this.height
         );
         ctx.fill();
+
+        ctx.restore();
     }
 }
