0fe5e6abeaf46481ffd8f923f80bb4c34b87f71a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-01 15:59:19 -0400

    created create polygon private method on the car class

--- car.js
+++ car.js
@@ -17,7 +17,31 @@ class Car{
 
     update(roadBorders){
         this.#move();
+        this.polygon = this.#createPolygon();
         this.sensor.update(roadBorders);
+    }
+
+    #createPolygon(){
+        const points=[];
+        const rad = Math.hypot(this.width,this.height)/2;
+        const alpha = Math.atan2(this.width, this.height);
+        points.push({
+            x:this.x-Math.sin(this.angle-alpha)*rad,
+            y:this.y-Math.cos(this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(this.angle+alpha)*rad,
+            y:this.y-Math.cos(this.angle+alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(Math.Pi+this.angle-alpha)*rad,
+            y:this.y-Math.cos(Math.Pi+this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(Math.Pi+this.angle+alpha)*rad,
+            y:this.y-Math.cos(Math.Pi+this.angle+alpha)*rad
+        });
+        return points;
     }
 
     #move(){
