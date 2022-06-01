8d4200931f9760e6a0256791bc1e760cf318e136
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-01 13:40:24 -0400

    implemented a function to retrieve the ray with the closest encounter

--- sensor.js
+++ sensor.js
@@ -6,10 +6,40 @@ class Sensor {
         this.raySpread = Math.PI/2;
 
         this.rays=[];
+        this.readings = [];
     }
 
-    update(){
+    update(roadBorders){
         this.#castRays();
+        this.readings=[];
+        for(let i=0; i< this.rays.length; i++){
+            this.readings.push(
+                this.#getReading(this.rays[i], roadBorders)
+            );
+        }
+    }
+
+    #getReading(ray, road){
+        let touches = [];
+
+        for(let i=0; i<roadBorders.length;i++){
+            const touch = getIntersection(
+                ray[0],
+                ray[1],
+                roadBorders[i][0],
+                roadBorders[i][1]
+            );
+            if(touch){
+                touches.push(touch);
+            }
+        }
+        if(touches.length === 0){
+            return null;
+        } else {
+            const offsets = touches.map(e => e.offset);
+            const minOffset = Math.min(...offsets);
+            return touches.find(e => e.offset === minOffset);
+        }
     }
 
     #castRays(){
