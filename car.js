a91f50082e3bae709340cc6c7e2b2fd1e3e33929
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-09 10:22:51 -0400

    restored commented code to the car file that momentarily fixes the disappering car bug

--- car.js
+++ car.js
@@ -81,6 +81,13 @@ class Car{
     }
 
     draw(ctx){
+        ctx.beginPath();
+        ctx.moveTo(this.polygon[0].x,this.polygon[0].y);
+        for(let i=1;i<this.polygon.length;i++){
+            ctx.lineTo(this.polygon[i].x,this.polygon[i].y);
+        }
+        ctx.fill();
+
         ctx.save();
         ctx.translate(this.x,this.y);
         ctx.rotate(-this.angle);
